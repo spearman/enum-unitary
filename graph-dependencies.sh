e3cb1ae0d37910851e33dc78d65a1ecc85925956
Shane Pearman <spearman@gitlab.com>
2019-08-24 22:26:54 -0700

    fix graph dependencies

--- graph-dependencies.sh
+++ graph-dependencies.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -x
 
-#cargo deps --optional-deps --no-transitive-deps 2> /dev/null > dependencies.dot \
-cargo deps --no-transitive-deps 2> /dev/null > dependencies.dot \
+cargo deps 2> /dev/null > dependencies.dot \
   && dot -Tpng dependencies.dot > dependencies.png \
   && feh dependencies.png
 
